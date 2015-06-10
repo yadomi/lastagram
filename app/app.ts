585d021f5e835f8f8b09830464c0940e82a243b1
Bastien Calou <bastien.calou@gmail.com>
2015-06-10 10:06:26 +0200

    basic component

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,14 @@
 /// <reference path="../typings/tsd.d.ts" />
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'app'
+})
+@View({
+  template: '<h1>Welcome !</h1>'
+})
+class App {
+
+}
+
+bootstrap(App);
