03b37c3c0aea5844f14093458ee1510a95f8ee87
Felix Yadomi <dev.yadomi@gmail.com>
2015-06-27 23:19:36 +0200

    Add Picture components

---
+++ app/components/instagram-picture/instagram-picture.ts
@@ -0,0 +1,11 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'instagram-picture'
+})
+@View({
+  templateUrl: './instagram-picture.html'
+})
+class InstagramPicture {
+
+}
