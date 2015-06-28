cd56649b7f4eb560511ca7c92de5a710f24536ba
Felix Yadomi <dev.yadomi@gmail.com>
2015-06-28 20:47:12 +0200

    Added PicturesList component

---
+++ app/components/instagram-pictures/instagram-pictures.js
@@ -0,0 +1,11 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'instagram-pictures'
+})
+@View({
+  templateUrl: './instagram-pictures.html'
+})
+class InstagramPictures {
+
+}
