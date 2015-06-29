6e2cc2e77946c0ebc0c13cb0123955ae57334389
Felix Yadomi <dev.yadomi@gmail.com>
2015-06-29 22:41:51 +0200

    Load Instagram image based on hashtag

--- app/components/instagram/pictures/component.ts
+++ app/components/instagram/pictures/component.ts
@@ -1,15 +1,34 @@
-import {Component, View} from 'angular2/angular2';
+import {Component, View, NgFor} from 'angular2/angular2'
+import {InstagramClient} from 'webservices/api-instagram'
+
+import {InstagramPicture} from 'components/instagram/picture/component'
+
 
 @Component({
-  selector: 'instagram-pictures'
+  selector: 'instagram-pictures',
+  appInjector: [InstagramClient]
 })
 @View({
-  templateUrl: 'components/instagram/pictures/template.html'
+  templateUrl: 'components/instagram/pictures/template.html',
+  directives: [NgFor, InstagramPicture]
 })
 export class InstagramPictures {
 
-  constructor(){
-    
+  private instagram: InstagramClient
+  private pictures: Array<Object>
+
+  constructor(instagram: InstagramClient) {
+    this.instagram = instagram;
+    this.init();
+  }
+
+  init() {
+    this.instagram.getMedias('pdb2015').then(this.onData.bind(this))
+  }
+
+  onData(res){
+    console.log(res);
+    this.pictures = res.data;
   }
 
 }
